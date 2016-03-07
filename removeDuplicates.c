12640974cd0b32a29ced702c24bfb84452ad3b8c
lilei8630 <lilei_8630@163.com>
2016-03-07 21:59:43 +0800

    2016.3.7

--- removeDuplicates.c
+++ removeDuplicates.c
@@ -1,2 +1,29 @@
 #include <stdio.h>
-void removeDuplicates
+#include <stdlib.h>
+#include <string.h>
+/*remove the duplicate characters in a string without using any additional buffer*/
+void removeDuplicates(char * str) {
+        if(str == NULL) return;
+        int len = strlen(str);
+        if(len < 2 ) return;
+        int tail = 1;
+        for(int i =1;i<len;i++){
+            int j;
+            for(j=0;j<tail;++j){
+                if(str[i] == str[j]) break;
+            }
+            if(j==tail){
+                str[tail] = str[i];
+                ++tail;
+            }
+        }
+        str[tail] = 0;
+}
+int main(){
+        char str[] = "";
+        removeDuplicates(str);
+        printf("%s\n",str);
+}
+
+
+
